79b5821e916378edc087965fbe7c54c6f2d06442
carriveaut <35465890+carriveaut@users.noreply.github.com>
2018-02-21 11:56:09 -0600

    Added user login auth

--- TicketPortal/settings.py
+++ TicketPortal/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+#Email Sending to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
