1ff0c2474573f3f0076581ca06d6f5dbdcdc21fc
tarynrachelx3 <35539894+tarynrachelx3@users.noreply.github.com>
2018-03-05 10:56:11 -0600

    Apps added: Shopping Cart & Tickets

--- ShoppingCart/apps.py
+++ ShoppingCart/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class ShoppingcartConfig(AppConfig):
+class ShoppingCartConfig(AppConfig):
     name = 'ShoppingCart'
--- TicketPortal/settings.py
+++ TicketPortal/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Home.apps.HomeConfig',
     'Tickets.apps.TicketsConfig',
-    'ShoppingCart.apps.ShoppingcartConfig',
+    'ShoppingCart.apps.ShoppingCartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
