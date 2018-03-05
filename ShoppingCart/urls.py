38e30099df0d4279eb375b471aa9d1080bf9a5b1
tarynrachelx3 <35539894+tarynrachelx3@users.noreply.github.com>
2018-03-05 10:17:09 -0600

    Apps added: Shopping Cart & Tickets

--- ShoppingCart/urls.py
+++ ShoppingCart/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'ShoppingCart'
 
 urlpatterns = [
-
+    path('', views.shoppingcart, 'shoppingcart'),
 ]
--- TicketPortal/settings.py
+++ TicketPortal/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Home.apps.HomeConfig',
     'Tickets.apps.TicketsConfig',
+    'ShoppingCart.apps.ShoppingcartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
