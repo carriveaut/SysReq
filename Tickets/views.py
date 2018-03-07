74280300fea51d6d0162ab7551386ceb3dd75b29
tarynrachelx3 <35539894+tarynrachelx3@users.noreply.github.com>
2018-03-07 11:54:25 -0600

    New: Table template

--- Tickets/views.py
+++ Tickets/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django_tables2 import tables
+from .tables import TicketTable
 
 
 def sports(request):
-    return render(request, 'Tickets/sports.html')
+    data = [{'event': 'Wild', 'location': 'Saint Cloud State', 'time': '8:00'},
+            {'event': 'Vikings', 'location': 'Target Field', 'time': '12:00'},
+            {'event': 'Twins', 'location': 'Target Field', 'time': '1:30'}]
+
+    table = TicketTable(data)
+
+    return render(request, 'Tickets/sports.html', {'table': table})
 
 
 def concerts(request):
