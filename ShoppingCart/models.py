0da2a02969453664833600984befe9547fbc2c50
tarynrachelx3 <35539894+tarynrachelx3@users.noreply.github.com>
2018-03-07 12:31:07 -0600

    New: Changing something.

--- ShoppingCart/models.py
+++ ShoppingCart/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Cart(models.Model):
     cartID = models.AutoField(primary_key=True)
-    ticketID = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    ticketID = models.ForeignKey('Tickets.Ticket', on_delete=models.CASCADE)
     qty = models.BigIntegerField
     price = models.FloatField
     name = models.CharField(max_length=50)
