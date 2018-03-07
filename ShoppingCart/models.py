2532a5fe529d75a437451117db3a6d2d9b325e41
carriveaut <35465890+carriveaut@users.noreply.github.com>
2018-03-07 11:24:21 -0600

    Added models

--- ShoppingCart/models.py
+++ ShoppingCart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Tickets import models
 
 # Create your models here.
+class Cart(models.Model):
+    ticketID = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    qty = models.BigIntegerField
+    price = models.FloatField
+    name = models.CharField(max_length=50)
+    date = models.DateTimeField
+    desc = models.CharField(max_length=250)
+    img = models.ImageField
--- Tickets/models.py
+++ Tickets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Ticket(models.Model):
+    ticketID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=250)
