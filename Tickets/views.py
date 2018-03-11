1f78d292bc31140c6069f9267fe0826fe0b37c4a
Tristan <zv3215wg@go.minnstate.edu>
2018-03-11 14:44:19 -0500

    Commit for consuming ticket master api and viewing stored tickets. See Tickets/views.py and test template

--- Tickets/views.py
+++ Tickets/views.py
@@ -6,6 +6,7 @@ import requests
 import ticketpy
 import json
 
+from .models import Ticket
 
 def sports(request):
     data = [{'event': 'Wild', 'location': 'Saint Cloud State', 'time': '8:00'},
@@ -26,38 +27,58 @@ def arttheater(request):
 
 
 def test(request):
+    # code for querying db
+    # currently filters by sports
+    concertlist = []
+    tickets = Ticket.objects.filter(classification="Sports")
+    for ticket in tickets:
+        concertlist.append(ticket)
 
-    tm_client = ticketpy.ApiClient('3dRZUZyfxeZ1U6EP8BajzFCol7ZAtSFb')
-    name = ''
-    pages = tm_client.events.find(
-        classification_name='Rock',
-        state_code='MN',
-        start_date_time='2018-01-01T20:00:00Z',
-        end_date_time='2019-01-01T20:00:00Z'
-    )
-    concert = []
-    vID = ''
-    venueName = []
-    var3 = []
-    # tick = Ticket()
-    for page in pages:
-        for event in page:
-            concert.append(event.name)
-            # tick = Ticket()
-            # name = event.name
-            # tick.event = name
-            # tick.venues = 'Test Venue'
-            # tick.start_Date = '2018-01-01'
-            # tick.price_Range = 30-50
-            # tick.status = 'onsale'
-            # tick.classification = 'Test Classification'
-            # tick.save()
-            # vID = event.id
-            # pages2 = tm_client.venues.find(
-            #     id=vID
-            # )
-            # for page2 in pages2:
-            #     for v in page2:
-            #         venueName.append(v.name)
-    return render(request, 'Tickets/test.html', {'name': concert})
+    # uncomment the following to add all sporting events for the year
+
+    # tm_client = ticketpy.ApiClient('3dRZUZyfxeZ1U6EP8BajzFCol7ZAtSFb')
+    #
+    # pages = tm_client.events.find(
+    #     classification_name='Sports',
+    #     state_code='MN',
+    #     start_date_time='2018-01-01T20:00:00Z',
+    #     end_date_time='2019-01-01T20:00:00Z'
+    # )
+    #
+    # concertlist = []
+    #
+    # for page in pages:
+    #     for event in page:
+    #         ticket = Ticket()
+    #         # print(event)
+    #         # print(event.__dict__.keys())
+    #         print("Name: ", event.name)
+    #         ticket.event = event.name
+    #
+    #         print("Venues: ", event.venues[0])
+    #         ticket.venues = event.venues[0]
+    #
+    #         print("Date: ", event.local_start_date)
+    #         ticket.start_Date = event.local_start_date
+    #
+    #         print("Status: ", event.status)
+    #         ticket.status = event.status
+    #
+    #         print("Classification: ", event.classifications[0].segment)
+    #         ticket.classification = event.classifications[0].segment
+    #
+    #         if not event.price_ranges:
+    #             print("Event has no price range")
+    #             ticket.price_Range = "No Price Range"
+    #
+    #         else:
+    #             print("Min Price: ", event.price_ranges[0].get('min'))
+    #             print("Max Price: ", event.price_ranges[0].get('max'))
+    #             price_range = str(event.price_ranges[0].get('min')) + "-" + str(event.price_ranges[0].get('max'))
+    #             ticket.price_Range = price_range
+    #         print("")
+    #         ticket.save()
+    #         concertlist.append(ticket)
+
+    return render(request, 'Tickets/test.html', {'name': concertlist})
     # return render(request, 'Tickets/test.html', {'name': concert}, {'venue': venueName})
