991e7b8570c193f85a717faa0c5bbb9855e599cb
tarynrachelx3 <35539894+tarynrachelx3@users.noreply.github.com>
2018-04-16 10:19:28 -0500

    Update: added tables to admin.

--- Tickets/admin.py
+++ Tickets/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Ticket
-
+from .models import Ticket, Order, OrderDetail, Checkout
 # Register your models here.
 admin.site.register(Ticket)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
+admin.site.register(Checkout)
